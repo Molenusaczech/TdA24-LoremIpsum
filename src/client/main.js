2bda187b940017f90cfca36bea526271806d43c0
Molenusaczech <06krtek@gmail.com>
2023-11-07 19:48:37 +0100

    test

--- src/client/main.js
+++ src/client/main.js
@@ -4,18 +4,7 @@ import { setupCounter } from "./counter.js";
 import javascriptLogo from "./javascript.svg";
 import { JsxTest } from "./component";
 
-import { renderLecturer } from "./pages/lecturer/renderer";
-import { lecturerAfter } from "./pages/lecturer/afterRender";
-import { defaultLecturer } from "./defaultLecturer";
-
-import { renderMain } from "./pages/main/renderer";
-import { mainAfter } from "./pages/main/afterRender";
 import { changeFavicon } from "./setFavicon.js";
-import { getLectorName } from "./getLectorName.js";
-
-import { renderNotFoundPage } from "./pages/notFound/renderer.js";
-import { notFoundAfter } from "./pages/notFound/afterRender.js";
-import { renderLoading } from "./pages/loading/renderer.js";
 
 import { renderPage } from "./routing.js";
 
