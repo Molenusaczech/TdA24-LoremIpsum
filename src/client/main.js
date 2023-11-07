f2d313ec275595d86571f2f9103988c4eb44f51e
Molenusaczech <06krtek@gmail.com>
2023-11-07 19:49:01 +0100

    remove some unused stuff

--- src/client/main.js
+++ src/client/main.js
@@ -1,8 +1,5 @@
 import "./style.css";
 
-import { setupCounter } from "./counter.js";
-import javascriptLogo from "./javascript.svg";
-import { JsxTest } from "./component";
 
 import { changeFavicon } from "./setFavicon.js";
 
