16fe9ddfc33970f528d20f70d32e69cb32e1c99d
Molenusaczech <06krtek@gmail.com>
2023-11-30 18:10:22 +0100

    first frontend for lecturers

--- src/client/pages/lecturer/renderer.js
+++ src/client/pages/lecturer/renderer.js
@@ -1,54 +1,72 @@
+import vlevoDole from "../../img/Tvar_vlevo_dole.png";
+import vpravoDole from "../../img/Tvar_vpravo_dole.png";
+import vpravoNahore from "../../img/Tvar_pravo_nahore.png";
+import { getLectorName } from "../../getLectorName";
+
 function renderLecturer(lecturer) {
     return /*html */`
+    <div class="lecturerPage">
     <button id="backButton">Back</button>
-    <h1> Lecturer </h1>
-    <ul>
-        <li> ${lecturer.UUID} </li>
-        <li> ${lecturer.title_before} </li>
-        <li> ${lecturer.first_name} </li>
-        <li> ${lecturer.middle_name} </li>
-        <li> ${lecturer.last_name} </li>
-        <li> ${lecturer.title_after} </li>
-        <li> ${lecturer.location} </li>
-        <li> ${lecturer.claim} </li>
-        <li> ${lecturer.bio} </li>
-        <li> ${lecturer.price_per_hour} </li>
-    </ul>
+    <div class="lectorContainer">
+    <div class="flexbox">
 
-    Tags:
-    <ul>
+    <div class="lectorLeftBox">
+
+    <img class="lectorPicture" id="lectorPic" src="${lecturer.picture_url}" alt="Lecturer picture">
+    <div class="lectorTags">
     ${lecturer.tags.map(renderTag).join("")}
-    </ul>
+    </div>
 
-    Phone numbers:
-    <ul>
+    </div>
+    <div class="lectorRightBox">
+    <div class="lectorName"> ${getLectorName(lecturer)} </div>
+    <hr>
+    <h2 class="lectorLocation"> ${lecturer.location} </h2>
+    <h3 class="lectorClaim"> ${lecturer.claim} </h3>
+    <div class="lectorBio"> ${lecturer.bio} </div>
+
+    <div>
+    </div>
+
+    <div class="lectorContact">
+    <div class="contactBox">
+    <span class="lectorContactOption"> ${lecturer.price_per_hour} Kč / hodina </span>
     ${lecturer.contact.telephone_numbers.map(renderPhoneNumber).join("")}
-    </ul>
-
-    Emails:
-    <ul>
     ${lecturer.contact.emails.map(renderEmail).join("")}
-    </ul>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
 
-    <img src="${lecturer.picture_url}" alt="Lecturer picture">
+
+    <img class="vlevoDole" src="${vlevoDole}">
+    <img class="vpravoDole" src="${vpravoDole}">
+    <img class="vpravoNahore" src="${vpravoNahore}">
+
+    </div>
     `;
 }
 
 function renderTag(tag) {
     return /*html */`
-    <li data-uuid="${tag.uuid}"> ${tag.name} </li>
+    <span class="lectorTag" data-uuid="${tag.uuid}"> ${tag.name} </span>
     `;
 }
 
 function renderPhoneNumber(phoneNumber) {
     return /*html */`
-    <li> ${phoneNumber} </li>
+    <a href="tel:${phoneNumber.replace(/\s/g, '')}">
+    <span class="lectorContactOption"> ${phoneNumber} </span>
+    </a>
     `;
 }
 
 function renderEmail(email) {
     return /*html */`
-    <li> ${email} </li>
+    <a href="mailto:${email}">
+    <span class="lectorContactOption"> ${email} </span>
+    </a>
     `;
 }
 
