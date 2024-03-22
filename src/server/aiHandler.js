b9f7b94feb0f08d209e15f7df3e17f331304ed96
Daniel Rajtšlégr <06krtek@gmail.com>
2024-03-22 18:28:17 +0100

    upsík

--- src/server/aiHandler.js
+++ src/server/aiHandler.js
@@ -11,20 +11,25 @@ async function aiResp(prompt) {
     // jen pro test frontendu ODSTRSANIT!!!!!
     //return "Test testus sus amogus";
 
-    const completion = await openai.chat.completions.create({
-        messages: [
-            {
-                role: "system", content: "You are a helpful assistant."
-            },
-            {
-                role: "user", content: prompt
-            }
-        ],
-        model: "gpt-3.5-turbo",
-    });
+    try {
+        const completion = await openai.chat.completions.create({
+            messages: [
+                {
+                    role: "system", content: "You are a helpful assistant."
+                },
+                {
+                    role: "user", content: prompt
+                }
+            ],
+            model: "gpt-3.5-turbo",
+        });
 
-    console.log(completion.choices[0]);
-    return completion.choices[0].message.content;
+        console.log(completion.choices[0]);
+        return completion.choices[0].message.content;
+    } catch (error) {
+        console.error(error);
+        return "Upsík, tohle mělo fungovat.";
+    }
 }
 
 export { aiResp };
